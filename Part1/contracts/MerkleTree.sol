64934a7f370b8bca7ba602591c360c4bd774e4f8
Mobius <chunloved@gmail.com>
2022-07-26 04:52:22 +0800

    All Finished except FE Bonus

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -8,13 +8,50 @@ contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
+    uint256 public layer;   // layer amount
+    uint256 public leafCnt; // inserted leaf amount
 
     constructor() {
         // [assignment] initialize a Merkle tree of 8 with blank leaves
+        layer = 3;
+        leafCnt = 8;
+        hashes = new uint256[](2*leafCnt-1);
+
+        for (uint i = 0; i < leafCnt-1; i++) {
+            hashes[leafCnt+i] = PoseidonT3.poseidon([hashes[2 * i],
+                                                     hashes[2 * i + 1]]);
+        }
+
+        root = hashes[hashes.length-1];
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
         // [assignment] insert a hashed leaf into the Merkle tree
+        // insert a hashed leaf into the Merkle tree
+        hashes[index] = hashedLeaf;
+        uint256 traversed;
+        uint cntBase;
+        uint256 layerIndex = index;
+
+        // calculate hash through parent path
+        for (uint i = 0; i < layer; i++) {
+            cntBase = layerIndex - traversed;
+            traversed += 2 ** (layer-i);
+            uint256 nextLayerIndex = cntBase / 2 + traversed;
+
+            if (layerIndex % 2 == 0) {
+                hashes[nextLayerIndex] = PoseidonT3.poseidon([hashes[layerIndex], hashes[layerIndex + 1]]);
+            } else {
+                hashes[nextLayerIndex] = PoseidonT3.poseidon([hashes[layerIndex - 1], hashes[layerIndex]]);
+            }
+
+            layerIndex = nextLayerIndex;
+        }
+
+        root = hashes[hashes.length-1];
+        index++;
+
+        return root;
     }
 
     function verify(
@@ -25,5 +62,7 @@ contract MerkleTree is Verifier {
         ) public view returns (bool) {
 
         // [assignment] verify an inclusion proof and check that the proof root matches current root
+        // verifies the inclusion proof and output root is the same as the one on chain
+        return verifyProof(a, b, c, input) && input[0] == root;
     }
 }
